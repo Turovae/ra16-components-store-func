f794acac8cee46388378cd93434411b6722aa867
Turovae <ekaterina.turova@mail.ru>
2023-05-06 12:31:21 +0700

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://turovae.github.io/ra16-components-store-func/',
   plugins: [react()],
 })
